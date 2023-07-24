0f875dd60977c07c3e9f6b03a1ed7a8c5cba7bb5
fbanespo1 <35040191+fbanespo1@users.noreply.github.com>
2023-07-23 23:10:37 -0400

    Create video.py

---
+++ video.py
@@ -0,0 +1,33 @@
+#pip install youtube-transcript-api, langchain, tiktoken, streamlit
+
+from langchain.document_loaders import YoutubeLoader
+from langchain import OpenAI
+from langchain.chains.summarize import load_summarize_chain 
+from langchain.text_splitter import CharacterTextSplitter
+
+import streamlit as st
+# Display the Page Title
+st.title('🦜️🔗 Video Summarizer :red[Toolbox]')
+
+import os
+api_key = st.text_input("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")
+
+if api_key:
+    os.environ["OPENAI_API_KEY"] = api_key
+
+    llm = OpenAI(temperature=0) #Temp controls the randomness of the text
+
+    prompt = st.text_input("Paste the URL")
+
+    if prompt:
+        loader = YoutubeLoader.from_youtube_url(prompt, add_video_info=False)
+
+        docs = loader.load()
+
+        text_splitter = CharacterTextSplitter(chunk_size=1000, separator = " ", chunk_overlap=50)
+
+        split_docs = text_splitter.split_documents(docs)
+
+        chain = load_summarize_chain(llm, chain_type="map_reduce")
+        
+        st.write(chain.run(split_docs))
